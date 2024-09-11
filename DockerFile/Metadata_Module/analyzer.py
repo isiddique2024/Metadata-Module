e6e4760cdcb7b43dc658850e47dfd0f3544252b0
akenox13 <akenowilliam2016@fau.edu>
2024-09-10 20:40:29 -0400

    Setting Up template for analyzer algo

---
+++ DockerFile/Metadata_Module/analyzer.py
@@ -0,0 +1,10 @@
+# Akeno Williams
+# def txt_analyzer (location_of_summary_txt: str, location_of_image_txt: str) -> list: {
+
+#in here we will load text files from the location, extract relationships and pass to dbOpertations to create grpahNodes and edges and output to an array of strings 
+
+#do we need to create an function to extract the mission profile of the digital twin and update DT object in DB
+
+
+#structure (DigitalTwin -> Relation -> DigitalTwin, DigitalTwin -> Relation -> LearnerNode)
+#arrayRelations["Jet -> powerplant-> GEJet", "GEJet -> learnerObject -> LearNode"]
