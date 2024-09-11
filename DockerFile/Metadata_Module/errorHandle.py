d2629e32f7bba21802379742138d944cfe309d8c
dylanjhaw <dhawryluk2020@fau.edu>
2024-09-10 21:30:34 -0400

    Added errorHandle.py file to work on error handling

---
+++ DockerFile/Metadata_Module/errorHandle.py
@@ -0,0 +1,6 @@
+#-----------------------------------Dylan--------------------------------------------#
+
+#build error handler if there is an error, collect log and put details in the details variable and call the messageBuilder Function
+
+#from statusFeed import statusFeed
+
